b0c8c6188f2ed0a42c0431df157eab5aba730f13
KarnLZM <40416454+KarnLZM@users.noreply.github.com>
2019-06-02 11:08:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,9 @@ echo "alias stopss='ssserver -c ~/ss.json -d stop'" >> ~/.bash_profile
 source ~/.bash_profile
 curl https://raw.githubusercontent.com/KarnLZM/ss/master/ss.json?token=Ami0xusZxm9zs3VlJ7KYLvy2GdOxDPVOks5bOHdfwA%3D%3D --output ~/ss.json
 
-uname -r
+yum update
 
+uname -r
 # 如果当前内核版本低于 4.10，可使用 ELRepo 源更新：
 sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
@@ -20,18 +21,23 @@ rpm -qa | grep kernel
 
 sudo egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
 
-sudo grub2-set-default 1
+sudo grub2-set-default 0  
 reboot
 
 uname -r
 
-echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf
-echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
+echo 'net.core.default_qdisc=fq' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
+
+# 添加优化内核参数
+vim /etc/systcl
+
 sudo sysctl -p
 
 sudo sysctl net.ipv4.tcp_available_congestion_control
 sudo sysctl -n net.ipv4.tcp_congestion_control
 lsmod | grep bbr
+
 
 sudo dd if=/dev/zero of=500mb.zip bs=1024k count=500
 
